7df609679cba7c3a2093fce50937ffdf053bf5b1
Megan Horsfield <meganthadev@gmail.com>
2020-04-27 00:32:04 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,7 +32,8 @@ class ApplicationController < Sinatra::Base
 
 	post "/login" do
 	  user = User.find_by(username: params[:username]
-	  
+	  if user && user.authenticate(params[:password])
+	    
 
 	get "/success" do
 		if logged_in?
