8140ccf0a93e26d7c8e004a701fad08c4c37b732
Megan Horsfield <meganthadev@gmail.com>
2020-04-27 00:29:15 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,8 +30,17 @@ class ApplicationController < Sinatra::Base
 		erb :login
 	end
 
+	post "/login" do
+    user = User.find_by(:username => params[:username])
+
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect to "/account"
+		else
+			redirect to "/failure"
+		end
+  end
 	
-	end
 
 	get "/failure" do
 		erb :failure
